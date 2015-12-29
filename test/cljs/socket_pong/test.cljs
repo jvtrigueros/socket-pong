5372a1d5ca55740ec65f96bcafb122646425ed2b
Jose V. Trigueros <j.v.trigueros@gmail.com>
2015-12-29 16:12:59 -0700

    test out the fack that the ball has collided on the right side

--- test/cljs/socket_pong/test.cljs
+++ test/cljs/socket_pong/test.cljs
@@ -1,7 +1,25 @@
 (ns ^:figwheel-load socket-pong.test
-  (:require [cljs.test :refer-macros [deftest is run-tests]]))
+  (:require [cljs.test :refer-macros [deftest is run-tests]]
+            [socket-pong.app :as app]))
 
-(deftest test-paddle-collision
-  (is (= 1 0) "life is good"))
+(enable-console-print!)
 
-(run-tests)
+(deftest test-player-paddle-collision
+  (let [paddle-id :player
+        state (as-> app/state-schema s
+                  (app/set-initial-state s 300 500)
+                  (update-in s [:ball] assoc :x (- (+ (:x (app/paddle s paddle-id)) (:width (app/paddle s paddle-id))) 1)
+                                             :y (:y (app/paddle s paddle-id))))
+
+        {px :x py :y ph :height pw :width} (app/paddle state paddle-id)
+        {bx :x by :y bdx :dx r :radius} (:ball state)
+
+        paddle-right-edge (+ px (/ pw 2))
+        paddle-top-edge (- py (/ ph 2))
+        paddle-bottom-edge (+ py (/ ph 2))
+        ball-left-edge (- bx r)
+        ball-top-edge (- by r)
+        ball-bottom-edge (+ by r)]
+    (is (and
+          (< paddle-top-edge ball-top-edge ball-bottom-edge paddle-bottom-edge)
+          (< (- paddle-right-edge (Math/abs bdx)) ball-left-edge paddle-right-edge)))))
