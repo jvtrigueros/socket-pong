2f1abb4d76ccecd6c10aa599908af7b82a403fe2
Jose V. Trigueros <j.v.trigueros@gmail.com>
2015-12-29 15:31:27 -0700

    set up project to allow test running from figwheel

--- test/cljs/socket_pong/test.cljs
+++ test/cljs/socket_pong/test.cljs
@@ -1,2 +1,7 @@
-(ns socket-pong.test
+(ns ^:figwheel-load socket-pong.test
   (:require [cljs.test :refer-macros [deftest is run-tests]]))
+
+(deftest test-paddle-collision
+  (is (= 1 0) "life is good"))
+
+(run-tests)
