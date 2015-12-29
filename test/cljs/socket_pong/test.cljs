54abe3fb8467b3831ad76b57dc9faa020d52a779
Jose V. Trigueros <j.v.trigueros@gmail.com>
2015-12-29 16:28:54 -0700

    clearly define where the ball should be

--- test/cljs/socket_pong/test.cljs
+++ test/cljs/socket_pong/test.cljs
@@ -8,8 +8,12 @@
   (let [paddle-id :player
         state (as-> app/state-schema s
                   (app/set-initial-state s 300 500)
-                  (update-in s [:ball] assoc :x (- (+ (:x (app/paddle s paddle-id)) (:width (app/paddle s paddle-id))) 1)
-                                             :y (:y (app/paddle s paddle-id))))
+                  (update-in s [:ball] assoc :x (- (+
+                                                     (:x (app/paddle s paddle-id))
+                                                     (/ (:width (app/paddle s paddle-id)) 2)
+                                                     (get-in s [:ball :radius]))
+                                                   1)
+                             :y (:y (app/paddle s paddle-id))))
 
         {px :x py :y ph :height pw :width} (app/paddle state paddle-id)
         {bx :x by :y bdx :dx r :radius} (:ball state)
