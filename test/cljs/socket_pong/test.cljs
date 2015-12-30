914791ce4281320a4cb09dd391bb280dea3eeaa7
Jose V. Trigueros <j.v.trigueros@gmail.com>
2015-12-29 18:06:25 -0700

    test collision on the left side of the paddle

--- test/cljs/socket_pong/test.cljs
+++ test/cljs/socket_pong/test.cljs
@@ -1,6 +1,7 @@
 (ns ^:figwheel-load socket-pong.test
   (:require [cljs.test :refer-macros [deftest is run-tests]]
-            [socket-pong.app :as app]))
+            [socket-pong.app :as app]
+            [cljs.pprint :as pp]))
 
 (enable-console-print!)
 
@@ -27,3 +28,29 @@
     (is (and
           (< paddle-top-edge ball-top-edge ball-bottom-edge paddle-bottom-edge)
           (< (- paddle-right-edge (Math/abs bdx)) ball-left-edge paddle-right-edge)))))
+
+(deftest test-enemy-paddle-collision
+  (let [paddle-id :enemy
+        state (as-> app/state-schema s
+                  (app/set-initial-state s 300 500)
+                  (update-in s [:ball] assoc :x (+ (-
+                                                     (:x (app/paddle s paddle-id))
+                                                     (/ (:width (app/paddle s paddle-id)) 2)
+                                                     (get-in s [:ball :radius]))
+                                                   1)
+                             :y (:y (app/paddle s paddle-id))))
+
+        {px :x py :y ph :height pw :width} (app/paddle state paddle-id)
+        {bx :x by :y bdx :dx r :radius} (:ball state)
+
+        paddle-right-edge (+ px (/ pw 2))
+        paddle-left-edge (- px (/ pw 2))
+        paddle-top-edge (- py (/ ph 2))
+        paddle-bottom-edge (+ py (/ ph 2))
+        ball-left-edge (- bx r)
+        ball-right-edge (+ bx r)
+        ball-top-edge (- by r)
+        ball-bottom-edge (+ by r)]
+    (is (and
+          (< paddle-top-edge ball-top-edge ball-bottom-edge paddle-bottom-edge)
+          (< paddle-left-edge ball-right-edge (+ paddle-left-edge (Math/abs bdx)))))))
